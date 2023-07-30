c86f212d5e4ecb52833e4de7ad6fd18cc9cdecb1
shutterbug2109 <nddesh1431@gmail.com>
2023-07-30 11:16:12 +0200

    ml flow and model evaluation added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from MLFlowProject.pipeline.stage_01_data_ingestions import DataIngestionTrainin
 from MLFlowProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from MLFlowProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from MLFlowProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-# from MLFlowProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+from MLFlowProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 # to test data ingestion stage delete the data folder and run this file
@@ -55,13 +55,13 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Model evaluation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = ModelEvaluationTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
